42c855b56d633455fa3dc5be655ddd412d637163
Sergey Zabolotnov <sergey.zabolotnov@gmail.com>
2013-09-26 20:11:21 +0400

    update capfile & deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,11 +3,10 @@ require 'undev/capistrano'
 set :application, 'shapado'
 
 set :scm, :git
+
 # TODO поменять репозитарий и ветку
 set :repository, 'git@github.com:zabolotnov87/shapado.git'
 set :branch, 'update_for_deploy'
-
-# set :asset_packager, 'jammit'
 
 set :use_sudo, false
 set :undev_ruby_version, '1.9.3-p327'
@@ -26,66 +25,38 @@ role :app, '10.40.56.86'
 role :db,  '10.40.56.86', :primary => true
 
 namespace :bundle do
-  task :install_binstubs do
+  desc 'Install binstubs'
+  task :install_binstubs, roles: :app do
     run "cd #{release_path} && bundle install --deployment --binstubs --path #{shared_path}/bundle --without development test"
   end
 end
 
-before "deploy:assets:precompile", "bundle:install_binstubs"
+namespace :deploy do
+  desc 'Create symlinks'
+  task :create_symlinks, roles: :app do
+    run "ln -nfs #{release_path}/config/shapado.yml.sample #{release_path}/config/shapado.yml"
+    run "ln -nfs #{release_path}/config/mongoid.yml.sample #{release_path}/config/mongoid.yml"
+  end
 
-#  namespace :deploy do
-#     task :restart, :roles => :app, :except => { :no_release => true } do
-#      run "echo '#{`git describe`}' > #{current_path}/public/version.txt"
-#      run "cd #{current_path} && ln -sf #{shared_path}/config/auth_providers.yml #{current_path}/config/auth_providers.yml"
-#
-#      assets.compass
-#      assets.package
-#
-#      #magent.restart
-#      bluepill.restart
-#
-#      run "rm -rf #{current_path}/tmp/cache/*"
-#    end
-#  end
+  # TODO: вынести в rake task + навести красоту
+  desc 'Update geoip base'
+  task :update_geoip, roles: :app do
+    run "rm -f #{release_path}/shared/GeoLiteCity.dat.gz"
+    run "mkdir -p #{release_path}/shared"
+    run "cd #{release_path}/shared && wget -q http://www.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz && gunzip -f GeoLiteCity.dat.gz"
+  end
 
-#  require 'ricodigo_capistrano_recipes'
-#
-#  set(:websocket_remote_config) { "#{shared_path}/config/pills/websocket.pill"}
-#  namespace :websocket do
-#    desc "setup websocket pill"
-#    task :setup do
-#      generate_config("#{File.dirname(__FILE__)}/pills/websocket.pill.erb", websocket_remote_config)
-#    end
-#
-#    desc "Init websocket with bluepill"
-#    task :init do
-#      rvmsudo "bluepill load #{websocket_remote_config}"
-#    end
-#
-#    desc "Start websocket with bluepill"
-#    task :start do
-#      rvmsudo "bluepill websocket start"
-#    end
-#
-#    desc "Restart websocket with bluepill"
-#    task :restart do
-#      websocket.stop
-#      websocket.start
-#    end
-#
-#    desc "Stop websocket with bluepill"
-#    task :stop do
-#      rvmsudo "bluepill websocket stop"
-#    end
-#
-#    desc "Display the bluepill status"
-#    task :status do
-#      rvmsudo "bluepill websocket status"
-#    end
-#
-#    desc "Stop websocket and quit bluepill"
-#    task :quit do
-#      rvmsudo "bluepill websocket stop"
-#      rvmsudo "bluepill websocket quit"
-#    end
-#  end
+  desc 'Compile & package assets'
+  task :process_assets, roles: :app do
+    run "cd #{current_path} && bundle exec compass compile; true"
+    run "cd #{current_path} && bundle exec jammit; true"
+  end
+
+  desc 'restart unicorn'
+  task :restart_unicorn, roles: :app do
+    run "sudo /usr/bin/sv restart /etc/service/shapado_unicorn"
+  end
+end
+
+after 'deploy:update', 'bundle:install_binstubs', 'deploy:create_symlinks', 'deploy:update_geoip', 'deploy:process_assets'
+after 'deploy:restart', 'deploy:restart_unicorn'
